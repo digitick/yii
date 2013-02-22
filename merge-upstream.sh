9ddd3c629615a7f40bd48bb2b88f1711b7b2f793
ianaré sévi <ianare@gmail.com>
2013-02-22 22:46:34 +0100

    improve merge script

--- merge-upstream.sh
+++ merge-upstream.sh
@@ -4,7 +4,7 @@ git fetch yiisoft master:master
 
 git merge -n --no-commit master
 
-git rm -q -r --ignore-unmatch build demos docs requirements tests
-git status | grep "deleted by us:" | awk '{print $5}' | xargs git rm -q -r
+git rm -qrf --ignore-unmatch build demos docs requirements tests
+git status | grep "deleted by us:" | awk '{print $5}' | xargs git rm -qrf
 
 git status
